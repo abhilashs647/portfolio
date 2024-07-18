e04d9782b221ae2a062f3ea6215e310701330c19
abhilashs647 <abhilashs647@gmail.com>
2024-07-18 19:14:19 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["web Developer", "Java Developer", "Python Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
